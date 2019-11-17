954897c37b97bdd975964cd84664af2ba5c4b718
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-11-18 10:43:17 +1100

    Update project_cashflows.R

--- infrastructure/project_cashflows.R
+++ infrastructure/project_cashflows.R
@@ -18,7 +18,6 @@ agency_data <- read_csv(paste0(LSL_DATA, master_file)) ## corresponds to &filena
 retirement_data <- read_csv(paste0(LSL_VAL, retirement_file)) ## corresponds to LSLVAL.&Retfile data set and RETIREMENTRATE table
 
 # do some wrangling of the original data
-## this dataframe corresponds to the &filename.val data set
 agency_val <- agency_data %>% 
   select(-c(Duration, Exit_Rate))
 filter(!is.na(Birthdate), !is.na(APS_Start), str_sub(Super, 1L, 1L) %in% c("P", "C")) %>%
@@ -33,50 +32,44 @@ filter(!is.na(Birthdate), !is.na(APS_Start), str_sub(Super, 1L, 1L) %in% c("P", 
 # extract some variables (used in downstream analysis)
 R_min_age <- min(retirement_data$Age)
 R_max_age <- max(retirement_data$Age)
-#min_duration <- min(agency_data$Duration, na.rm = TRUE)
-#max_duration <- max(agency_data$Duration, na.rm = TRUE)
-#N_datarec <- nrow(agency_data)
-#C_minage <- min(agency_val$age)
-#laps <- R_maxage - C_minage
 
-# separate out the exit rate data from the rest of the input data
-# also define the takeup rates for each year of eligibility
+# define exit rates and takeup rates [retirement rates depend on super scheme]
 exit_rates <- agency_data %>% filter(complete.cases(agency_data)) %>% select(Exit_Rate) %>% unlist() 
 takeup_rates <- ifelse(start_rate - (0:100)*reduce_rate >= end_rate, start_rate - (0:100)*reduce_rate, end_rate)
 
-# for now let's just simplify things by assuming one set of retirement rates:
-retirement_rates <- retirement_data$Retirement_PSS %>% unlist()
 
 ## LOOK AT A GIVEN OBSERVATION
 current_record <- agency_val[1, ]
 
-# set up params for analysis
-start_age <- current_record$age
-start_service <- current_record$service
-LSL <- current_record$LSL
-analysis_length <- R_max_age - start_age + 1
-sum_taken <- 0
-age <- start_age
-service <- start_service
-vector(length = analysis_length) -> qx -> px # -> taken_this_year # etc
-# i think we need qx/px to be vectors because px is built up recursively out of qx's
-# dunno about the others
-px[1] <- 1
-# retirement_rates <- ifelse(current_record$Super == "PSS", retirement_data$PSS, retirement_data$CSS)
-
-while (age <= R_max_age) {
-  index <- age - start_age + 1
-  at_retirement_age <- age >= R_min_age
-  eligible <- at_retirement_age || service >= eligible_years
-  year_of_eligibility <- max(age - R_min_age + 1, service - eligible_year + 1)
-  if (at_retirement_age) qx[index] <- retirement_rates[age - R_min_age + 1] else qx[index] <- exit_rates[service + 1]
-  if (eligible) takeup_rate <- takeup_rates[year_of_eligibility] else takeup_rate <- 0
-  taken_this_year <- LSL*takeup_rate
-  LSL <- LSL - taken_this_year
-  # multiply by px to condition on survival: taken_this_year*px
-  # multiply LSL by px*qx for exit at this year: LSL*px*qx
-  # then i guess CF is just the sum of these
-  age <- age + 1
-  service <- service + 1
-  px[index + 1] <- px[index]*(1 - qx[index])
-}
+  # set up params for analysis
+  start_age <- current_record$age
+  start_service <- current_record$service
+  analysis_length <- R_max_age - start_age + 1
+  LSL <- current_record$LSL
+  sum_taken <- 0
+  age <- start_age
+  service <- start_service
+  qx <- px<- vector(length = analysis_length) # -> taken_this_year etc
+  px[1] <- 1
+  retirement_rates <- ifelse(str_sub(current_record$Super, 1L, 1L) == "P", 
+                             retirement_data$Retirement_PSS, 
+                             retirement_data$Retirement_CSS)
+  
+  # now project cash flows for each year
+  while (age <= R_max_age) {
+    index <- age - start_age + 1
+    at_retirement_age <- age >= R_min_age
+    eligible <- at_retirement_age || service >= eligible_years
+    year_of_eligibility <- max(age - R_min_age + 1, service - eligible_year + 1)
+    if (at_retirement_age) qx[index] <- retirement_rates[age - R_min_age + 1] else qx[index] <- exit_rates[service + 1]
+    if (eligible) takeup_rate <- takeup_rates[year_of_eligibility] else takeup_rate <- 0
+    taken_this_year <- LSL*takeup_rate
+    taken_this_year_weighted <- taken_this_year*px[index]
+    sum_taken <- sum_taken + taken_this_year
+    after_retirement <- LSL - sum_taken
+    after_retirement_weighted <- after_retirement*px[index]*qx[index]
+    # CF is just the sum of these:
+    age <- age + 1
+    service <- service + 1
+    px[index + 1] <- px[index]*(1 - qx[index])
+  }
